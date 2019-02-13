ee78b5cb6f02fc413552eb213b6eac96c1dcc904
jagdishkumar17 <jagdish.kumar@spanservices.com>
2019-02-13 10:51:09 +0530

    Update setting

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 * https://www.youtube.com/watch?v=Ke90Tje7VS0
 * https://www.tutorialspoint.com/reactjs/reactjs_components.htm
 * https://stackoverflow.com/questions/46878058/react-html-changing-the-color-of-what-has-been-selected
+
 ```
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.js";
+import Input from "./components/Forms/Input.jsx";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Input />, document.getElementById("root"));
