e97d32b141614eecdd911bc9697e5fd7f271d602
mougilz <pawan.modgil@spanservices.com>
2019-02-18 12:32:36 +0530

    unused link removed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Instance from "./app/components/App.jsx";
-// import Instance from "./app/components/Forms/keys.jsx";
-// import Instance from "./app/components/Forms/ref.jsx";
+// import Instance from "./app/components/Forms/input.jsx";
 // import Instance from "./app/components/Events/events.jsx";
 // import Instance from "./app/components/State/stateAndProps.jsx";
 // import Instance from "./app/components/ForceUpdate/forceUpdateAndsetState.jsx";
