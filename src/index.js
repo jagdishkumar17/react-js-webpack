ab6efb66555d51ea05d9a69687194679e51692d9
mougilz <pawan.modgil@spanservices.com>
2019-02-18 12:30:42 +0530

    default page set again

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import Instance from "./app/components/App.jsx";
+import Instance from "./app/components/App.jsx";
 // import Instance from "./app/components/Forms/keys.jsx";
 // import Instance from "./app/components/Forms/ref.jsx";
 // import Instance from "./app/components/Events/events.jsx";
 // import Instance from "./app/components/State/stateAndProps.jsx";
 // import Instance from "./app/components/ForceUpdate/forceUpdateAndsetState.jsx";
 // import Instance from "./app/components/ReactDom/reactDom.jsx";
- import Instance from "./app/components/LifeCycle/Lifecycle.jsx";
+// import Instance from "./app/components/LifeCycle/Lifecycle.jsx";
 
 ReactDOM.render(<Instance />, document.getElementById("root"));
 
