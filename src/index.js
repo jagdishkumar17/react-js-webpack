43f9ae0773da870f68fc3cd8a35568408352cc32
mougilz <pawan.modgil@spanservices.com>
2019-02-18 15:36:49 +0530

    code added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Instance from "./app/components/App.jsx";
+// import Instance from "./app/components/App.jsx";
 // import Instance from "./app/components/Forms/input.jsx";
-// import Instance from "./app/components/Events/events.jsx";
+import Instance from "./app/components/Events/events.jsx";
 // import Instance from "./app/components/State/stateAndProps.jsx";
 // import Instance from "./app/components/ForceUpdate/forceUpdateAndsetState.jsx";
 // import Instance from "./app/components/ReactDom/reactDom.jsx";
