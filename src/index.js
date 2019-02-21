dbe246ea6fa6f58bb3a5d655ccb20d53e5a46ac5
jagdishkumar17 <jagdish.kumar@spanservices.com>
2019-02-21 12:58:17 +0530

    Create sep service

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from 'react-router-dom';
 import App from "./app/components/App.jsx";
 import { IntlProvider, addLocaleData } from "react-intl";
 import translations from "./i18n/locales";
